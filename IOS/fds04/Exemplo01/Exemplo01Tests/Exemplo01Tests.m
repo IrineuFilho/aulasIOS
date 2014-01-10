772ac7d1f3363fed2bd87ade721d5b19aa2072bc
IrineuFilho <irineutenorio@gmail.com>
2014-01-10 19:19:37 -0300

    Initial Commit

---
+++ IOS/fds04/Exemplo01/Exemplo01Tests/Exemplo01Tests.m
@@ -0,0 +1,34 @@
+//
+//  Exemplo01Tests.m
+//  Exemplo01Tests
+//
+//  Created by Irineu Mauricio V T Filho on 10/01/14.
+//  Copyright (c) 2014 Irineu Mauricio V T Filho. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Exemplo01Tests : XCTestCase
+
+@end
+
+@implementation Exemplo01Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
